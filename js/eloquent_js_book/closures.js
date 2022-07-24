e18c89d1cccfc184f95d5e02fbf48e100eeef183
Andrei Popovici <popovici.andrey@gmail.com>
2022-07-24 15:20:24 +0300

    schoolf of FE & promises async await

---
+++ js/eloquent_js_book/closures.js
@@ -0,0 +1,40 @@
+/*
+It's about 2 functions - outer function & inner function
+Closures remember the outer function scope even after creation time.
+*/
+
+// function human() {
+//     const name = 'Marko'
+//     function sayHi() {
+//         console.log(`Hi I am ${name}`)
+//     }
+//     function sayHowYouFeel() {
+//         console.log(`${name} is feeling good`)
+//     }
+//     sayHi()
+//     sayHowYouFeel()
+// }
+
+function human(name) {
+    function sayHi() {
+        console.log(`Hi I am ${name}`)
+    }
+    function sayHowYouFeel() {
+        console.log(`${name} is feeling good`)
+    }
+
+    return {
+        sayHi,
+        sayHowYouFeel
+    }
+}
+
+//human()
+human('Eugene')
+
+const marko = human('Marko')
+const ted = human('Ted')
+
+marko.sayHi()
+ted.sayHowYouFeel()
+
