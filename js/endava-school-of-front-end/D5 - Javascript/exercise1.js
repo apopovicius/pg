0530e522b309d6a12aa9cbe3babdab6f8fa30255
Andrei Popovici <popovici.andrey@gmail.com>
2022-07-25 22:56:37 +0300

    Update exercise1.js
    reformat in vscode

--- js/endava-school-of-front-end/D5 - Javascript/exercise1.js
+++ js/endava-school-of-front-end/D5 - Javascript/exercise1.js
@@ -12,25 +12,23 @@ Example: getPhotoForStudent('Adrian') -> Reject with error message (The photo do
 
 function getPhotoForStudent(name) {
     return new Promise((resolve, reject) => {
-       setTimeout( () => {
-  			let students = ['Andrei', 'Alexandru', 'Claudiu', 'Cristiana', 'Florin', 'Sergiu'];
-     		let photo = students.filter(student => student === name)
-        									  .map(student => student + '.png');
-      	photo.length > 0 ? resolve(photo): reject('The photo doesn\'t exist');
-      }, 2000);
+        setTimeout( () => {
+            let students = ['Andrei', 'Alexandru', 'Claudiu', 'Cristiana', 'Florin', 'Sergiu'];
+            let photo = students.filter(student => student === name).map(student => student + '.png');
+            photo.length > 0 ? resolve(photo): reject('The photo doesn\'t exist');
+        }, 2000);
     });
 }
 
 getPhotoForStudent('Andrei')
-  .then(result => console.log(result))
-	.catch(error => console.log(error))
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
 
 
 getPhotoForStudent('Andreia')
-  .then(result => console.log(result))
-	.catch(error => console.log(error))
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
 
 getPhotoForStudent('Cristiana')
-  .then(result => console.log(result))
-	.catch(error => console.log(error))
-
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
