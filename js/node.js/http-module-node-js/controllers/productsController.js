95cfaa00caef927859946b191db05f1533720e4f
Andrei Popovici <popovici.andrey@gmail.com>
2022-08-30 16:44:17 +0300

    add update, delete functionality

--- js/node.js/http-module-node-js/controllers/productsController.js
+++ js/node.js/http-module-node-js/controllers/productsController.js
@@ -25,11 +25,12 @@ async function getProduct(req, res, id) {
         if (!product) {
             res.writeHead(404, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'Product Not Found' }));
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            res.end(JSON.stringify(product));
         }
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-        res.end(JSON.stringify(product));
     } catch (error) {
         console.error(error);
     }
@@ -64,8 +65,58 @@ async function createProduct(req, res) {
     }
 }
 
+// @desc Update a Product
+// @route PUT /api/products/:id
+async function updateProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Product Not Found' }));
+        } else {
+            const body = await getPostData(req);
+            const { title, description, price } = JSON.parse(body);
+            const productData = {
+                title: title || product.title,
+                description: description || product.description,
+                price: price || product.price,
+            };
+
+            const updatedProduct = await Product.update(id, productData);
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            res.end(JSON.stringify(updatedProduct));
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// @desc Delete Product
+// @route DELETE /api/products/:id
+async function deleteProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Product Not Found' }));
+        } else {
+            await Product.remove(id);
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            res.end(JSON.stringify({ message: `Product ${id} removed` }));
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 module.exports = {
     getProducts,
     getProduct,
     createProduct,
+    updateProduct,
+    deleteProduct,
 };
