249b96941c74733e6dafb06abcb81d390fcd5f03
Andrei Popovici <popovici.andrey@gmail.com>
2022-09-06 17:46:30 +0300

    add source for the server handling

---
+++ js/node.js/todo-ejs-express/readme.md
@@ -0,0 +1,4 @@
+EJS - templating
+GET AND POST request
+
+https://www.youtube.com/watch?v=dyImiV6MGqg
---
+++ js/node.js/todo-with-modules/app.js
@@ -0,0 +1,50 @@
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+//MW
+app.use(express.json()); // parse json bodies in the request object
+
+// logs
+app.use(morgan('tiny'));
+
+// parse request to body parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set('view engine', 'ejs');
+// if there is no views folder(default get by ejs)
+// app.set('views', ')
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+
+//Global error handler. IMP function params MUST start with err
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    console.log(err.name);
+    console.log(err.code);
+
+    res.status(500).json({
+        message: 'Ups... Something went wrong!',
+    });
+});
+
+// load routers
+//app.use('/', require('./server/routes/router'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//listen on part
+dotenv.config('.env');
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'http:://localhost';
+app.listen(PORT, () => console.log(`Server running on ${HOST}:${PORT}`));
