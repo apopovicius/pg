a762dbcfef48e4a34bbbf4e5c93e7db0643abe1e
Andrei Popovici <popovici.andrey@gmail.com>
2022-12-09 12:19:08 +0200

    day 8 challenge

--- js/xmass-challenge/day5-candyshop.js
+++ js/xmass-challenge/day5-candyshop.js
@@ -1,16 +1,16 @@
 /*
    It's the day after Halloween 🎃 and all the candy is on sale!
-   
+
    To buy up all the candy, use map() and filter() to put all the
-   candy into a `shoppingCart` array. 
-   
+   candy into a `shoppingCart` array.
+
    The new array should contain only the item and the price, like
-   this: 
-   
-   Expected output: 
+   this:
+
+   Expected output:
    [
        {item: "🍭", price: 2.99},
-       {item: "🍫", price: 1.99}, 
+       {item: "🍫", price: 1.99},
        {item: "🍬", price: 0.89}
     ]
 */
