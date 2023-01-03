e02a1a76bf3e02605b31804353bbeac69257e565
Andrei Popovici <popovici.andrey@gmail.com>
2023-01-03 19:04:00 +0200

    add 2 new katas

---
+++ js/codewars/13.validParantheses.js
@@ -0,0 +1,24 @@
+/*
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+*/
+
+function validParentheses(parens) {
+    let open = 0;
+    for (p of parens) {
+        if (p === '(') open++;
+        if (p === ')') open--;
+        if (open < 0) return false; // includes check when string starts with `)`
+    }
+    return open === 0 ? true : false;
+}
+
+console.log(validParentheses('()'));
+console.log(validParentheses(')(()))'));
+console.log(validParentheses('('));
+console.log(validParentheses('(())((()())())'));
