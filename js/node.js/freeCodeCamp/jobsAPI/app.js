8a431e6c614970024d0acc65e6995b833d6919e2
Andrei Popovici <popovici.andrey@gmail.com>
2023-02-09 01:16:09 +0200

    project done by adding swagger ui'

--- js/node.js/freeCodeCamp/jobsAPI/app.js
+++ js/node.js/freeCodeCamp/jobsAPI/app.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 require('express-async-errors');
 
+//extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -8,20 +19,34 @@ const app = express();
 const notFoundMW = require('./middleware/not-found');
 const errorHandlerMW = require('./middleware/error-handler');
 
-// routers
-const authRoute = require('./routes/auth');
-const jobsRoute = require('./routes/jobs');
+// mw
+const authenticationMW = require('./middleware/autentication');
 
 // DB
 const connectDB = require('./db/connect');
 
+// routers
+const authRoute = require('./routes/auth');
+const jobsRoute = require('./routes/jobs');
+
 app.use(express.json());
 
 //extra packages
+app.set('trust proxy', 1);
+app.use(rateLimiter({ windowMs: 15 * 60 * 100 /*15 min*/, max: 100 }));
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //routes
+app.get('/', (req, res) =>
+    res.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`)
+);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/jobs', jobsRoute);
+app.use('/api/v1/jobs', authenticationMW, jobsRoute);
 
 app.use(notFoundMW);
 app.use(errorHandlerMW);
--- js/node.js/freeCodeCamp/jobsAPI/controllers/jobs.js
+++ js/node.js/freeCodeCamp/jobsAPI/controllers/jobs.js
@@ -1,21 +1,78 @@
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({ job });
+};
+
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs');
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort(
+        'createdAt'
+    );
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getOneJob = async (req, res) => {
-    res.send('get one job');
-};
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req;
 
-const createJob = async (req, res) => {
-    res.send('create job');
+    const job = await Job.find({
+        _id: jobId,
+        createdBy: userId,
+    });
+    if (!job) {
+        throw new NotFoundError(`No job found with id: ${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({ job });
 };
 
 const updateJob = async (req, res) => {
-    res.send('update job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+        body: { company, position },
+    } = req;
+
+    if (company === '' || position === '') {
+        throw new BadRequestError('Company or position cannot be empty');
+    }
+
+    const job = await Job.findOneAndUpdate(
+        {
+            _id: jobId,
+            createdBy: userId,
+        },
+        req.body,
+        { new: true, runValidators: true }
+    );
+
+    if (!job) {
+        throw new NotFoundError(`No job found with id: ${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-    res.send('delete job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+
+    const job = await Job.findOneAndDelete({
+        _id: jobId,
+        createdBy: userId,
+    });
+
+    if (!job) {
+        throw new NotFoundError(`No job found with id: ${jobId}`);
+    }
+
+    res.status(StatusCodes.OK).send(`Job ${job} was deleted`);
 };
 
 module.exports = { getAllJobs, getOneJob, createJob, updateJob, deleteJob };
