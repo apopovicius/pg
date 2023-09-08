c1110471695f8b3b11eeebb2c417603ba55955fa
Andrei Popovici <popovici.andrey@gmail.com>
2023-09-08 12:34:04 +0300

    update nvim config

---
+++ config/nvim/lua/maps.lua
@@ -0,0 +1,74 @@
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+local status, telescope = pcall(require, "telescope.builtin")
+if status then
+	-- Telescope
+	map("n", "<leader>ff", telescope.find_files)
+	map("n", "<leader>fg", telescope.live_grep)
+	map("n", "<leader>fb", telescope.buffers)
+	map("n", "<leader>fh", telescope.help_tags)
+	map("n", "<leader>fs", telescope.git_status)
+	map("n", "<leader>fc", telescope.git_commits)
+else
+	print("Telescope not found")
+end
+
+-- <leader> = the space key
+
+map("n", "<leader>pv",":Ex<CR>")
+    
+-- Save
+map("n", "<leader>w", "<CMD>update<CR>")
+
+-- Quit
+map("n", "<leader>q", "<CMD>q<CR>")
+
+-- Exit insert mode
+map("i", "jk", "<ESC>")
+
+-- Windows
+map("n", "<leader>sv", "<CMD>vsplit<CR>")
+map("n", "<leader>sh", "<CMD>split<CR>")
+
+-- NeoTree
+map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
+map("n", "<leader>o", "<CMD>Neotree focus<CR>")
+
+-- Buffer
+map("n", "<TAB>", "<CMD>bnext<CR>")
+map("n", "<S-TAB>", "<CMD>bprevious<CR>")
+
+-- Terminal
+map("n", "<leader>th", "<CMD>ToggleTerm size=10 direction=horizontal<CR>")
+map("n", "<leader>tv", "<CMD>ToggleTerm size=80 direction=vertical<CR>")
+
+-- Markdown Preview
+map("n", "<leader>m", "<CMD>MarkdownPreview<CR>")
+map("n", "<leader>mn", "<CMD>MarkdownPreviewStop<CR>")
+
+-- Window Navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-j>", "<C-w>j")
+
+-- Resize Windows
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
+
+-- Debugger
+  --map("n", "<F5>", require 'dap'.continue)
+  map("n", "<C-b>", "<CMD>lua require'dap' .toogle_breakpoint()<CR>")
+  --map("n", "<C-l>", function() 
+    --require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition'))
+ -- end)
+
+  --map("n", "<F10>", require 'dap'.step_over)
+  --map("n", "<F11>", require 'dap'.step_into)
+  --map("n", "<F12>", require 'dap'.step_out)
+  --map("n", "<F6>", require 'dap'.repl.open)
+  --map("n", "dl", require 'dap'.run_last)
