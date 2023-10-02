9781535b7efbf668cc975db75b524c0075a2d006
Andrei Popovici <popovici.andrey@gmail.com>
2023-10-02 14:06:14 +0300

    implement profile card

---
+++ js/react/04-profile-card/src/components/Content.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Content() {
+  return (
+    <section className="content">
+      <h1 className="content--header">About</h1>
+      <p className="content--text">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+      <h1 className="content--header">Interest</h1>
+      <p className="content--text">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </section>
+  );
+}
