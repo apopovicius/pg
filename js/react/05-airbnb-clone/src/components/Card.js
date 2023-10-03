7563a2185f150ce8c1427e550aaa02580b423801
Andrei Popovici <popovici.andrey@gmail.com>
2023-10-03 16:51:00 +0300

    completed airbnb clone project

--- js/react/05-airbnb-clone/src/components/Card.js
+++ js/react/05-airbnb-clone/src/components/Card.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 export default function Card(props) {
+  console.log(props);
+  //let { openSpots, location, coverImg, stats, title, price } = props.card; -> this is for Card V3
+  let { openSpots, location, coverImg, stats, title, price } = props;
+
+  let badgeText = "";
+  if (openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-      <img src={`../images/${props.img}`} className="card--image" />
+      {badgeText !== "" && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${coverImg}`} className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.country}</span>
+        <span>{stats.rating}</span>
+        <span className="gray">({stats.reviewCount}) • </span>
+        <span className="gray">{location}</span>
       </div>
-      <p>{props.title}</p>
-      <p>
-        <span className="bold">From ${props.price}</span> / person
+      <p className="card--title">{title}</p>
+      <p className="card--price">
+        <span className="bold">From ${price}</span> / person
       </p>
     </div>
   );
