a1b017f510bd196d7fa091cf82781c4cea28a8cf
Andrei Popovici <popovici.andrey@gmail.com>
2023-10-25 20:07:26 +0300

    add decorators to dtos so that it can be displayed on swagger

--- js/nest.js/demo-swagger/src/career/dto/create-career-dto.ts
+++ js/nest.js/demo-swagger/src/career/dto/create-career-dto.ts
@@ -1,18 +1,42 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class CreateCareerDto {
+  @ApiProperty({
+   example: 'company-name',
+   required: true
+  })
   @IsString()
   readonly company: string;
 
+  @ApiProperty({
+   example: 'dev, tester',
+   required: true
+  })
   @IsString()
   readonly role: string;
 
+  @ApiProperty({
+   example: '2021',
+   required: true,
+   description: 'this is the starting year of employement'
+  })
   @IsNumber()
   readonly year: number;
 
+  @ApiProperty({
+   example: '5',
+   required: true,
+   description: 'number of years spend on this company'
+  })
   @IsNumber()
   readonly duration: number;
 
+  @ApiProperty({
+    example: '["C++", "java", "swift"]',
+    required: true,
+    description: 'this is a list of skills'
+  })  
   @IsString({each: true})
   readonly skills: string[];
 }
