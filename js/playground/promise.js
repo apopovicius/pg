d0b32188744b2471611ebafd401baba5674429e2
Andrei Popovici <popovici.andrey@gmail.com>
2023-10-26 16:00:06 +0300

    play with promise

---
+++ js/playground/promise.js
@@ -0,0 +1,20 @@
+const myPromise = (ms, retVal) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(retVal);
+        }, ms);
+    });
+};
+
+async function main() {
+    const retVal = await myPromise(500, 10);
+    console.log(retVal);
+}
+
+async function main2() {
+    console.log(await main());
+}
+
+console.log(0);
+main2();
+console.log(1);
