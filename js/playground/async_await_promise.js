bec03d625fe366c459c5e1781075fc21c2341aeb
Andrei Popovici <popovici.andrey@gmail.com>
2023-11-09 15:58:10 +0200

    small adjs

--- js/playground/async_await_promise.js
+++ js/playground/async_await_promise.js
@@ -1,21 +1,21 @@
 const f_async = async (n) => {
   return n;
-}
+};
 
 const f_async_promise = async (ms) => {
   const promise = new Promise((resolve) => {
     setTimeout(() => {
-      resolve(ms);      
+      resolve(ms);
     }, ms);
   });
-  
+
   let result = await promise;
   return result;
-}
+};
 
 console.log("p1-start");
 console.log(f_async(100));
-console.log(/*await*/f_async_promise(500)); // syntax error: await is available ONLY in async functions or top-level modules
+console.log(/*await*/ f_async_promise(500)); // syntax error: await is available ONLY in async functions or top-level modules
 console.log("p1-stop");
 
 // await can be used on its own with JS modules
@@ -23,11 +23,10 @@ console.log("p1-stop");
   console.log("p2-start");
   console.log(await f_async_promise(500));
   console.log("p2-stop");
-}) ();
-
+})();
 
 async function wait() {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return 10;
 }
 
@@ -35,18 +34,18 @@ function f() {
   // ...what should you write here?
   // we need to call async wait() and wait to get 10
   // remember, we can't use "await"
-  wait().then(result => console.log(result));
-}; 
+  wait().then((result) => console.log(result));
+}
 
 f();
 
 /*
  * async functions always return a Promise
  * await only works inside async functions (syntax error)
- * await on top level works fine inside a module. 
+ * await on top level works fine inside a module.
  * if using old browser and not using modules use wrapping into an annonymus functions(line 22)
- * await accept "thenables" 
- * 
+ * await accept "thenables"
+ *
  * class Thenable {
  * constructor(num) {
  *   this.num = num;
@@ -66,8 +65,8 @@ f();
  *
  * If await gets a non-promise object with .then,
  * it calls that method providing the built-in functions resolve and reject as arguments
- * (just as it does for a regular Promise executor). 
- * Then await waits until one of them is called 
+ * (just as it does for a regular Promise executor).
+ * Then await waits until one of them is called
  * (in the example above it happens in the line (*)) and then proceeds with the result.
  *
  * The await keyword before a promise makes JavaScript wait until that promise settles, and then: * 1. If it’s an error, an exception is generated — same as if throw error were called at that very place.
