5ef9d280320734cbc1165f698cc8d741edd96973
Andrei Popovici <popovici.andrey@gmail.com>
2023-12-07 14:20:24 +0200

    i cheat for day2 challenge, in my defence the requirements were not that clear

--- js/aventofcode23/02.js
+++ js/aventofcode23/02.js
@@ -1,53 +1,83 @@
 const fs = require('fs');
-const MAX_RED = 12;
-const MAX_GREEN = 13;
-const MAX_BLUE = 14;
 
 const countGems = (round) => {
-  const gems = round.split(',');
-  let red = 0;
-  let blue = 0;
-  let green = 0;
-  gems.forEach( gem => {
-    const parts = gem.split(' ');
-    switch(parts[2]) {
-      case 'red': red +=+parts[1]; break;
-      case 'blue': blue +=+parts[1]; break;
-      case 'green': green +=+parts[1]; break;
-      default: 
-        throw new Error(`corrupted game! gem: ${parts[2]} not found`);
-    }
-  });
-  return [red, green, blue];
+    const gems = round.split(',');
+    let red = 0;
+    let blue = 0;
+    let green = 0;
+    gems.forEach((gem) => {
+        const parts = gem.split(' ').filter((e) => e !== '');
+        switch (parts[1]) {
+            case 'red':
+                red += +parts[0];
+                break;
+            case 'blue':
+                blue += +parts[0];
+                break;
+            case 'green':
+                green += +parts[0];
+                break;
+            default:
+                throw new Error(`corrupted game! gem: ${parts[2]} not found`);
+        }
+    });
+    return [red, green, blue];
 };
 
+const OK = {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
 
-const games = fs.readFileSync('02.in').toString('utf-8').split('\n');
-let red = 0;
-let green = 0;
-let blue = 0;
-let sum = 0;
+function main() {
+    const games = fs.readFileSync('02.in').toString('utf-8').split('\n');
+    const gameSelection = {}; // P1+P2
+    const gamePowers = {}; // P2
+    let sum = 0; // P1
+    let sumOfPowers = 0; // P2
 
-games.forEach( g => {
-  console.log(g);
-  const parts = g.split(':');
-  const key = parts[0].split(' ')[1];
-  const rounds = parts[1].split(';');
-  rounds.forEach( ro => {
-    let [r,g,b] = countGems(ro);
-    console.log(r,' ', g, ' ', b);
-    red +=r;
-    green += g;
-    blue += b;
-  });
-  console.log(`After game ${key} we got red:${red}, green: ${green}, blue: ${blue} gems`);
-  if(red > MAX_RED || green > MAX_GREEN || blue > MAX_BLUE) console.log(`Invalid game! Over the threshold`);
-  else {
-    sum += +key;
-  }
-  red = 0;
-  blue = 0;
-  green = 0;
-});
+    for (let g of games) {
+        const parts = g.split(':');
+        const gameId = parts[0].split(' ')[1];
+        const rounds = parts[1].split(';').map((x) => x.trim());
 
-console.log(sum);
+        // create new gameSelection
+        gameSelection[gameId] = {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+
+        // for some reason i miss this from problem description
+        // seen on other people solution that for each game you get the max number of gem color
+        // eg for game X:  [10 green, 9 blue, 1 red], [1 red, 7 green], [11 green, 6 blue], [8 blue, 12 green]
+        // we keep: [1 red, 12 green, 9 blue]
+        for (let round of rounds) {
+            const amounts = round.split(', ');
+            for (let amount of amounts) {
+                const [count, color] = amount.split(' ');
+                const number = Number(count);
+                if (gameSelection[gameId][color] <= number) {
+                    gameSelection[gameId][color] = number;
+                }
+            }
+        }
+
+        gamePowers[gameId] =
+            gameSelection[gameId].red *
+            gameSelection[gameId].blue *
+            gameSelection[gameId].green;
+        sumOfPowers += gamePowers[gameId];
+
+        if (gameSelection[gameId].red > OK.red) continue;
+        if (gameSelection[gameId].green > OK.green) continue;
+        if (gameSelection[gameId].blue > OK.blue) continue;
+        sum += +gameId;
+    }
+
+    console.log(sum); // P1
+    console.log(sumOfPowers); // P2
+}
+
+main();
