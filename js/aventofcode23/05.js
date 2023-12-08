4105b8ccaead804289094f1976f25e21a7949105
Andrei Popovici <popovici.andrey@gmail.com>
2023-12-08 22:32:11 +0200

    first attempt 05.js

--- js/aventofcode23/05.js
+++ js/aventofcode23/05.js
@@ -1,78 +1,62 @@
-const assert = require("assert");
-const fs = require("fs");
-const almanac = fs.readFileSync("05.in").toString("utf-8").split("\n\n");
+const fs = require('fs');
+const almanac = fs.readFileSync('05.in').toString('utf-8').split('\r\n\r\n');
 
-const parseMap = (map) => {
-  return map.split('\n').filter(e=>e!=='');
-}
-
-const checkRange = (line, number) => {
-  return +number;
-};
-
-const getSoilFromSeed = (seed) => {
-  const seed2soil = almanac[1].split(":")[1];
-  const lines = parseMap(seed2soil);
-  let soil = -1;
-  for(let line of lines) {
-    soil = checkRange(line, seed);
-    if(soil !== -1) break;
-  }
-  assert(soil !== -1);
-  return soil;
-};
-
-const getFertilizerFromSoil = (soil) => {
-  const soil2fertilizer = almanac[2].split(":")[1];
-};
-
-const getWaterFromFertilizer = (fertilizer) => {
-  const fertilizer2water = almanac[3].split(":")[1];
-};
-
-const getLightFromWater = (water) => {
-  const water2light = almanac[4].split(":")[1];
-};
-
-const getTempFromLight = (light) => {
-  const light2temp = almanac[5].split(":")[1];
-};
-
-const getHumidityFromTemp = (temp) => {
-  const temp2humidity = almanac[6].split(":")[1];
-};
-
-const getLocationFromHumidity = (humidity) => {
-  const humidity2location = almanac[7].split(":")[1];
+const getDestination = (map, target) => {
+    const lines = map.split('\r\n').filter((e) => e !== '');
+    for (let line of lines) {
+        const [start, destinationStart, step] = line
+            .split(' ')
+            .map((x) => Number(x));
+        const stop = start + step;
+        if (target >= start && target < stop) {
+            // console.log(
+            //     `[start: ${start}, target:${target}, stop: ${
+            //         start + step
+            //     }], [destStart: ${destination} ->  destination: ${
+            //         destination + target - start
+            //     }], [${target - start} = ${
+            //         destination + target - start - destination
+            //     }]`
+            // );
+            const destination = destinationStart + (target - start);
+            console.log(`${target} ==> ${destination}`);
+            return destination;
+        }
+    }
+    const destination = target;
+    //console.log(`*For ${target} returning: ${destination}`);
+    console.log(`${target} ==> ${destination}`);
+    return destination;
 };
 
 function main() {
-  const seeds = almanac[0].split(":")[1];
-  let min = -1;
-  seeds.split(' ').filter(e=> e!=='').forEach(seed => {
-    let output = '';
-    output += `Seed ${seed},`;
-    const soil = getSoilFromSeed(seed);
-    output += `soil ${soil},`;
-    const fertilizer = getFertilizerFromSoil(soil);
-    output += `fertilizer ${fertilizer},`;
-    const water = getWaterFromFertilizer(fertilizer);
-    output += `water ${water},`;
-    const light = getLightFromWater(water);
-    output += `light ${light},`;
-    const temp = getTempFromLight(light);
-    output += `temperature ${temp},`;
-    const humidity = getHumidityFromTemp(temp);
-    output += `humidity ${humidity},`;
-    const location = getLocationFromHumidity(humidity);
-    output += `location ${location}.`;
-    //console.log(output);
-    if(min === -1 || min > +location) {
-      min = +location;
-    }
-  });
-
-  console.log(min);
+    const seeds = almanac[0].split(':')[1];
+    let minLocation = [];
+    seeds
+        .split(' ')
+        .filter((e) => e !== '')
+        .forEach((seed) => {
+            let output = '';
+            output += `Seed ${seed}, `;
+            const soil = getDestination(almanac[1].split(':')[1], +seed);
+            output += `soil ${soil}, `;
+            const fertilizer = getDestination(almanac[2].split(':')[1], soil);
+            output += `fertilizer ${fertilizer},`;
+            const water = getDestination(almanac[3].split(':')[1], fertilizer);
+            output += `water ${water}, `;
+            const light = getDestination(almanac[4].split(':')[1], water);
+            output += `light ${light}, `;
+            const temp = getDestination(almanac[5].split(':')[1], light);
+            output += `temperature ${temp}, `;
+            const humidity = getDestination(almanac[6].split(':')[1], temp);
+            output += `humidity ${humidity}, `;
+            const location = getDestination(almanac[7].split(':')[1], humidity);
+            output += `location ${location}. `;
+            //console.log(output);
+            minLocation.push(location);
+        });
+    minLocation.sort((a, b) => a - b);
+    console.log(minLocation[0]);
 }
 
 main();
