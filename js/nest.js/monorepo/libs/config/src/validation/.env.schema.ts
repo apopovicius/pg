85be9b0c3c30e448b7dc686ede7c540bd740f4a5
Andrei Popovici <popovici.andrey@gmail.com>
2025-03-27 19:55:11 +0200

    test(demo): fix unit test

--- js/nest.js/monorepo/libs/config/src/validation/.env.schema.ts
+++ js/nest.js/monorepo/libs/config/src/validation/.env.schema.ts
@@ -1,5 +1,5 @@
 import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('dev', 'prod').default('dev'),
+  NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
 });
