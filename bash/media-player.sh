54a8904ee512cea73abdf139ca173fb92394f82c
Andrei Popovici <popovici.andrey@gmail.com>
2026-02-24 14:06:08 +0200

    feat(bash): add media-player script

--- bash/media-player.sh
+++ bash/media-player.sh
@@ -8,31 +8,31 @@ ICON_YOUTUBE="\uf166"
 max_lenght=60
 
 for player in $(playerctl -l); do
-    status=$(playerctl -p "$player" status 2>/dev/null)
-    if [ "$status" == "Playing" ]; then
-        artist=$(playerctl -p "$player" metadata artist 2>/dev/null)
-        title=$(playerctl -p "$player" metadata title 2>/dev/null)
-        url=$(playerctl -p "$player" metadata xesam:url 2>/dev/null)
-        short_title="${title:0:$max_lenght}"
+  status=$(playerctl -p "$player" status 2>/dev/null)
+  if [ "$status" == "Playing" ]; then
+    artist=$(playerctl -p "$player" metadata artist 2>/dev/null)
+    title=$(playerctl -p "$player" metadata title 2>/dev/null)
+    url=$(playerctl -p "$player" metadata xesam:url 2>/dev/null)
+    short_title="${title:0:$max_lenght}"
 
-        if [ -n "$title" ] || [ -n "$artist" ]; then
-            icon="$ICON_GENERIC" # generic icon
-            if [[ "$player" == "spotify" || "$url" == *"spotify"* ]]; then
-                icon="$ICON_SPOTIFY"
-            elif [[ "$url" == *"soundcloud"* ]]; then
-                icon="$ICON_SOUNDCLOUD"
-            elif [[ "$url" == *"youtube.com"* || "$url" == *"youtu.be"* ]]; then
-                icon="$ICON_YOUTUBE"
-            fi
-
-            song_info="$icon $artist - $short_title"
-            if [ "$icon" == "$ICON_YOUTUBE" ]; then
-                song_info="$icon $short_title" # no need to display the author of video in case of YT
-            fi
-            echo -e "$song_info"
-            exit 0
-        fi
+    if [ -n "$title" ] || [ -n "$artist" ]; then
+      icon="$ICON_GENERIC" # generic icon
+      if [[ "$player" == "spotify" || "$url" == *"spotify"* ]]; then
+        icon="$ICON_SPOTIFY"
+      elif [[ "$url" == *"soundcloud"* ]]; then
+        icon="$ICON_SOUNDCLOUD"
+      elif [[ "$url" == *"youtube.com"* || "$url" == *"youtu.be"* ]]; then
+        icon="$ICON_YOUTUBE"
+      fi
+      space=" "
+      song_info="$icon $space $artist - $short_title"
+      if [ "$icon" == "$ICON_YOUTUBE" ]; then
+        song_info="$icon $space $short_title" # no need to display the author of video in case of YT
+      fi
+      echo -e "$song_info"
+      exit 0
     fi
+  fi
 done
 
 echo -e "$song_info"
